bf06fc58c81a4579cc6f481fbb3861e4c3eb144e
mahmudsajeeb <saifurrahmansajib37@gmail.com>
2025-07-21 00:16:01 -0700

    vector size

--- vector.cpp
+++ vector.cpp
@@ -4,6 +4,13 @@ using namespace std;
  
 int main()
 {
-     
+    int a[6]={1,2,3,4,5,6};
+     vector<int> v(a,a+6);
+    
+     for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+     }
+     cout<<endl;
+     cout<<v.size()<<endl;
     return 0;
 }
